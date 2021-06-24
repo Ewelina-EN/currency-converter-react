d91b21663865a26090224478c28f7dde8a0f6c1c
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-24 20:58:34 +0200

    component header added

---
+++ src/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const Header = (props) => (
+    <legend className="legend">
+        KALKULATOR WALUTOWY
+    </legend>
+);
+
+export default Header;
