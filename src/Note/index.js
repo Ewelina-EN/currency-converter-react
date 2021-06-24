61732e27803eea3f9148327531d8837f73259805
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-24 21:00:37 +0200

    component note added

---
+++ src/Note/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const Note = (props) => (
+    <p className="note">
+        Pola z oznaczeniem * wymagają wypełnienia
+    </p>
+)
+
+export default Note;
