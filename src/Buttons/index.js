dff967d785b73f884b37b73294917bbedb138c42
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-24 21:00:20 +0200

    component buttons added

---
+++ src/Buttons/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+const Buttons = (props) => (
+    <>
+    <button className="button ">Przelicz</button>
+    <button type="reset" className="button">Wyczyść</button>
+    </>
+)
+
+export default Buttons;
