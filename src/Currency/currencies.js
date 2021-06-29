4374abe4970548a9280770f0c0a5621f11cb01a2
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-29 14:13:33 +0200

    added currencies file

---
+++ src/Currency/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 3.78,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.56,
+    },
+    {
+        short: "SEK",
+        name: "Korona szwedzka",
+        rate: 0.44,
+    },
+];
