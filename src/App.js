e8a905bfa20a4475a7c0f46207234cb340a15129
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-29 14:23:17 +0200

    added useState in App.js and changes in components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "./Header";
 import Amount from "./Amount";
 import Currency from './Currency';
 import Buttons from './Buttons';
 import Note from './Note';
 import Result from './Result';
+import { currencies } from './Currency/currencies';
 
 
 function App() {
+
+  const [result, setResult] = useState();
+  const [amount, setAmount] = useState();
+  const [currency, setCurrency] = useState(currencies[0].short);
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ short }) => short === currency)
+      .rate;
+
+    setResult({
+      targetAmount: amount / rate,
+      currency,
+    })
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  };
+
   return (
-    <body>
+    <form onSubmit={onSubmit} >
       <fieldset className="fieldset">
         <Header />
         <p>
-          <Amount />
+          <Amount
+            amount={amount}
+            setAmount={setAmount} />
         </p>
         <p>
-          <Currency />
+          <Currency
+            currency={currency}
+            setCurrency={setCurrency} />
         </p>
         <p>
-          <Buttons />
+          <Buttons
+            calculateResult={calculateResult} />
         </p>
       </fieldset>
       <Note />
-      <Result />
-    </body>
+      <Result
+        result={result} />
+    </form>
 
   );
 };
