ce814c8a053579f99a1e1ee2b899bd10a73e275b
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-30 12:31:23 +0200

    the option to show the result added

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
 
-const Result = ({ result }) => (
-    <p className="result">WYNIK:{result}
+const Result = ({result}) => (
+    // console.log(result));
+    <p className="result">
+        WYNIK: {result.targetAmount} {result.currency}
     </p>
-)
 
+)
 export default Result;
