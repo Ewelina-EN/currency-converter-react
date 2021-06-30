8dca7b9fa69b756d91645f25c9820fa38e2b00fb
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-30 12:55:43 +0200

    clean the result folder

--- src/Result/index.js
+++ src/Result/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./style.css";
 
 const Result = ({ result }) => (
-    // console.log(result));
     <p className="result">
         WYNIK: {result.targetAmount} {result.currency}
     </p>
