562d1caef593ad4687b795a7365647169d8e50df
Ewelina-EN <nieweglowska.ewelina@gmail.com>
2021-07-19 15:06:27 +0200

    date formatting transferred to clock component

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { StyledClock } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
+
+const formatDate = (date) => date.toLocaleDateString(undefined, {
+    day: "numeric",
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+});
+
 const Clock = () => {
     const date = useCurrentDate();
 
@@ -9,7 +20,7 @@ const Clock = () => {
         <StyledClock>
             Dzisiaj jest
             {" "}
-            {date}
+            {formatDate(date)}
         </StyledClock>
     );
 };
--- src/Clock/useCurrentDate.js
+++ src/Clock/useCurrentDate.js
@@ -1,23 +1,11 @@
 import { useState, useEffect } from "react";
 
-const formatDate = (date) => {
-    return date.toLocaleDateString(undefined, {
-        day: "numeric",
-        weekday: "long",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-    });
-};
-
 export const useCurrentDate = () => {
-    const [date, setDate] = useState(formatDate(new Date()));
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setDate(formatDate(new Date()));
+            setDate(new Date());
         }, 1000);
         return () => {
             clearInterval(intervalId);
