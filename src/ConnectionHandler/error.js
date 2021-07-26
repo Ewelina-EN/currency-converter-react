8ad9629b7c56b29b7a7c1984ecc695f4622feb01
Ewelina-EN <nieweglowska.ewelina@gmail.com>
2021-07-26 14:16:03 +0200

    create ConnectionHandler,add error/loading info

---
+++ src/ConnectionHandler/error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledConnectionHandler } from "./styled";
+
+const Error = (props) => (
+    <StyledConnectionHandler>
+        🛠️ Hmm... Coś poszło nie tak. Spróbuj jeszcze raz później...
+    </StyledConnectionHandler>
+);
+
+export default Error;
---
+++ src/ConnectionHandler/loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledConnectionHandler } from "./styled";
+
+const Loading = (props) => (
+    <StyledConnectionHandler>
+        Chwila cierpliwości...Ładuję kursy walut z Europejskiego Banku Centralnego ⏳
+    </StyledConnectionHandler>
+);
+
+export default Loading;
